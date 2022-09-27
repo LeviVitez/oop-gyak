e991b55bb61e6352fbfb7de63d878c23f7296efc
LeviVitez <bmwyolo555@gmail.com>
2022-09-27 20:27:11 +0200

    eddig

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-class Zene{
-    #nev
-    #hossz
+let zenék = [];
 
-constructor(nev,hossz){
-    this.#nev=nev;
-    this.#hossz=hossz;
+class zene {
+  #zene;
+  #length;
+
+  constructor(zene, length) {
+    this.#zene = zene;
+    this.#length = length;
+  }
+
+  get zene() {
+    return this.#zene;
+  }
+
+  get length() {
+    return this.#length;
+  }
 }
+
+function sum() {
+  var sum = 0;
+  for (let i = 0; i < zenék.length; i++) {
+    sum += zenék[i].length;
+  }
+  return sum;
 }
 
-let zenek=[];
-
-
-
-function hozzad() {
-    let nev= document.getElementById("nev").value;
-    let hossz= parseFloat(document.getElementById("hossz").value); 
-    console.log(nev)
-    console.log(hossz)
-        
-
-
-}   
+function addToList() {
+  var data = new zene(
+    document.getElementById("zene").value,
+    parseInt(document.getElementById("length").value)
+  );
+  zenék.push(data);
+  var sPlaceHolder = sum();
+  document.getElementById("sum").innerText =
+    "A zeneszámok össz hossza: " + sPlaceHolder + " perc";
+}
+function init() {
+  document.getElementById("add").addEventListener("click", addToList);
+}
+document.addEventListener("DOMContentLoaded", init);
+console.log(sum());
