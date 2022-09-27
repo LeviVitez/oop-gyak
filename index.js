3fda2aec5394905662cba6c21cbfca33ef3b46e2
Bóta László Levente <bmwyolo555@gmail.com>
2022-09-27 13:05:05 +0200

    fejlodes

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ constructor(nev,hossz){
 }
 }
 
-const zenek=[];
+let zenek=[
+    Zene.nev,
+    Zene.hossz
+];
 
-
+kiir(){
+    console.log(user.nev, user.eletkor);
+}
 function hozzad() {
-    let nev= document.getElementById("nev").textContent;
+    let nev= document.getElementById("nev").value;
     let hossz= parseFloat(document.getElementById("hossz").value); 
     console.log(nev)
     console.log(hossz)
+
+    new zenek.nev(nev)
+    new zenek.hossz(hossz)
+
+    console.log(zenek.nev)
+    console.log(zenek.hossz)
+
 }   
